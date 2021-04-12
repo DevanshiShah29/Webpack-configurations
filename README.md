1e80f0f55f9dd60200ebb8400a7cdd15cc7c36f6
Devanshi Shah <devanshishah@Devanshi.local>
2021-04-12 14:38:34 +0530

    advanced configurations

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Webpack-configurations
 This repo contains code for basic webpack configuration with splitting up development and production build modes as well as setting up a dev server.
+
+- [x] React project with webpack 
+- [x] Loaders: babel, HTMLloader, CSS, SCSS with autoPrefixer, Style loader
+- [x] Webpack dev server with hot loading
+- [x] Environment configuration
+- [x] Cache handling with seprate cache file for vendor
+- [x] Clean webpack build 
+- [x] Minified CSS 
+- [x] Webpack bundle analyzer 
+- [x] chunking
--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
 
   //entry file 
-  entry: "./src/index.js",
+  entry:{
+    main: "./src/index.js",
+    bootstrap: "./src/vendor.js",
+    vendor: ['react', 'react-dom'],
+  },
 
   //loaders
   module: {
@@ -15,10 +19,33 @@ module.exports = {
         use: { loader: "babel-loader" },
       },
       {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
       }
     ],
+  },
+  optimization: {
+    // https://stackoverflow.com/questions/48985780/webpack-4-create-vendor-chunk
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test:/[\\/]node_modules[\\/]/,
+          name: "other",
+          chunks: 'all',
+        },
+       
+      },
+    }
   },
   // Provides template to HTML file inside dist directory
   plugins: [
